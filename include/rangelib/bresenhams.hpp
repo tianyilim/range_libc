f727baf79218617cfd75043eb7fd38532ae1fab4
tianyilim <0.tianyi.lim@gmail.com>
2023-09-30 18:07:50 +0200

    overload

--- include/rangelib/bresenhams.hpp
+++ include/rangelib/bresenhams.hpp
@@ -10,7 +10,7 @@ class BresenhamsLine : public RangeMethod {
    public:
     BresenhamsLine(OMap m, float mr) : RangeMethod(m, mr){};
 
-    float calc_range(float x, float y, float heading)
+    float calc_range(float x, float y, float heading) const override
     {
         // first check if the cell underneath the query point is occupied, if so return
         if (_distTransform.isOccupied((int)x, (int)y)) {
