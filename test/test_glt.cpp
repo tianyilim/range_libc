eb883173312c3a9faa367db3e97aa920a750c5ea
tianyilim <0.tianyi.lim@gmail.com>
2023-10-07 18:33:31 +0200

    add more GLT tests

--- test/test_glt.cpp
+++ test/test_glt.cpp
@@ -4,53 +4,102 @@
 
 class GiantLUTTest : public ::testing::Test {
    protected:
-    void SetUp() override
-    {
-        ranges::WorldValues wv(0.5, 0, 0, 0);
+    void SetUp() override { GLTTestOMap = ranges::OMap(filename); }
 
-        BLTestOMap = ranges::OMap(filename);
-        BLTestOMap.setWorldValues(wv);
-    }
+    inline float degToRad(float r) { return r * M_PI / 180.0; }
 
     const std::string filename = "../../maps/tests/symm_box.png";
 
     const float maxRange = 25.0f;
     const unsigned thetaDiscretization = 90;
-    ranges::OMap BLTestOMap;
+    ranges::OMap GLTTestOMap;
 
-    ranges::WorldValues WorldVals;
+    // ranges::WorldValues WorldVals;
 };
 
 TEST_F(GiantLUTTest, SimpleConstructor)
 {
-    ranges::GiantLUTCast GiantLUT(BLTestOMap, maxRange, thetaDiscretization);
+    ranges::GiantLUTCast GiantLUT(GLTTestOMap, maxRange, thetaDiscretization);
 
     EXPECT_NO_THROW(GiantLUT.memory());
     EXPECT_NO_THROW(GiantLUT.report());
     EXPECT_FLOAT_EQ(GiantLUT.maxRange(), maxRange);
 
     // test equality of distance transform
-    EXPECT_EQ(GiantLUT.getMap().height(), BLTestOMap.height());
-    EXPECT_EQ(GiantLUT.getMap().width(), BLTestOMap.width());
-    EXPECT_EQ(GiantLUT.getMap().filename(), BLTestOMap.filename());
-    EXPECT_EQ(GiantLUT.getMap().worldValues(), BLTestOMap.worldValues());
+    EXPECT_EQ(GiantLUT.getMap().height(), GLTTestOMap.height());
+    EXPECT_EQ(GiantLUT.getMap().width(), GLTTestOMap.width());
+    EXPECT_EQ(GiantLUT.getMap().filename(), GLTTestOMap.filename());
+    EXPECT_EQ(GiantLUT.getMap().worldValues(), GLTTestOMap.worldValues());
+}
+
+TEST_F(GiantLUTTest, IndexTest)
+{
+    ranges::GiantLUTCast GiantLUT(GLTTestOMap, maxRange, thetaDiscretization);
+
+    EXPECT_EQ(GiantLUT.lutWidth(), 120);
+    EXPECT_EQ(GiantLUT.lutHeight(), 120);
+    EXPECT_EQ(GiantLUT.lutThetaDiscretization(), thetaDiscretization);
+    EXPECT_EQ(GiantLUT.lutArraySize(), 120 * 120 * thetaDiscretization);
+    EXPECT_EQ(GiantLUT.getMap().width() * GiantLUT.getMap().height() * thetaDiscretization,
+              GiantLUT.lutArraySize());
+
+    unsigned i1, i2;  // index counters
+
+    i1 = GiantLUT.getLutIdx(0, 0, 0);
+    i2 = GiantLUT.getLutIdx(0, 0, 1);
+    EXPECT_EQ(i1 + 1, i2);
+
+    i1 = GiantLUT.getLutIdx(0, 0, 0);
+    i2 = GiantLUT.getLutIdx(0, 1, 0);
+    EXPECT_EQ(i1 + thetaDiscretization, i2);
+
+    i1 = GiantLUT.getLutIdx(0, 0, 0);
+    i2 = GiantLUT.getLutIdx(1, 0, 0);
+    EXPECT_EQ(i1 + 120 * thetaDiscretization, i2);
+
+    i1 = GiantLUT.getLutIdx(119, 119, thetaDiscretization - 1);
+    EXPECT_EQ(i1, GiantLUT.lutArraySize() - 1);
+}
+
+TEST_F(GiantLUTTest, ThetaDiscretizeTest)
+{
+    ranges::GiantLUTCast GiantLUT(GLTTestOMap, maxRange, thetaDiscretization);
+
+    int res;
+    res = GiantLUT.discretize_theta(degToRad(0.0));
+    EXPECT_EQ(res, 0);
+    res = GiantLUT.discretize_theta(degToRad(90.0));
+    EXPECT_EQ(res, 23);
+    res = GiantLUT.discretize_theta(degToRad(-270.0));
+    EXPECT_EQ(res, 23);
+    res = GiantLUT.discretize_theta(degToRad(270.0));
+    EXPECT_EQ(res, 68);
+    res = GiantLUT.discretize_theta(degToRad(-90.0));
+    EXPECT_EQ(res, 68);
+    res = GiantLUT.discretize_theta(degToRad(180.0));
+    EXPECT_EQ(res, 45);
+    res = GiantLUT.discretize_theta(degToRad(360.0));
+    EXPECT_EQ(res, 0);
+    res = GiantLUT.discretize_theta(degToRad(319.25));
+    EXPECT_EQ(res, 80);
+    res = GiantLUT.discretize_theta(degToRad(370.0));
+    EXPECT_EQ(res, 3);
 }
 
 TEST_F(GiantLUTTest, RangeTest)
 {
-    ranges::GiantLUTCast GiantLUT(BLTestOMap, maxRange, thetaDiscretization);
+    ranges::GiantLUTCast GiantLUT(GLTTestOMap, maxRange, thetaDiscretization);
 
     EXPECT_NEAR(GiantLUT.calc_range(0, 0, 0), maxRange, 0.01);
     EXPECT_NEAR(GiantLUT.calc_range(0, 0, M_PIf / 2), maxRange, 0.01);
 
     // From edges of the square
-    EXPECT_NEAR(GiantLUT.calc_range(0, 21, 0), 21.0f / 2, 0.01);
-    EXPECT_NEAR(GiantLUT.calc_range(21, 0, M_PIf / 2), 21.0f / 2, 0.01);
-    EXPECT_NEAR(GiantLUT.calc_range(119, 21, M_PIf), 20.0f / 2, 0.01);
-    EXPECT_NEAR(GiantLUT.calc_range(21, 119, -M_PIf / 2), 20.0f / 2, 0.01);
+    EXPECT_NEAR(GiantLUT.calc_range(0, 21, 0), 21.0f, 0.01);
+    EXPECT_NEAR(GiantLUT.calc_range(21, 0, M_PIf / 2), 21.0f, 0.01);
+    EXPECT_NEAR(GiantLUT.calc_range(119, 21, M_PIf), 20.0f, 0.01);
+    EXPECT_NEAR(GiantLUT.calc_range(21, 119, -M_PIf / 2), 20.0f, 0.01);
 
     // angles pointing out of map
-    // THIS FAILS
     EXPECT_NEAR(GiantLUT.calc_range(0, 0, -M_PIf / 2), maxRange, 0.01);
     EXPECT_NEAR(GiantLUT.calc_range(119, 0, 0), maxRange, 0.01);
     EXPECT_NEAR(GiantLUT.calc_range(0, 119, M_PIf / 2), maxRange, 0.01);
@@ -62,9 +111,9 @@ TEST_F(GiantLUTTest, RangeTest)
     EXPECT_NEAR(GiantLUT.calc_range(120, 120, 0), maxRange, 0.01);
 }
 
-TEST_F(GiantLUTTest, OtherFunctionTest)
+TEST_F(GiantLUTTest, SensorModelSetTest)
 {
-    ranges::GiantLUTCast GiantLUT(BLTestOMap, maxRange, thetaDiscretization);
+    ranges::GiantLUTCast GiantLUT(GLTTestOMap, maxRange, thetaDiscretization);
 
     double sensorModel[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
     int tableWidth = 10;
